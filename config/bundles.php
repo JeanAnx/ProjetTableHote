f5b795a6775cb3432c408e7ff133e5ab7ea12a21
Danalieth <laurevois@gmail.com>
2019-01-07 15:59:15 +0100

    Fixtures des tables d'hôtes

--- config/bundles.php
+++ config/bundles.php
@@ -1,4 +1,5 @@
 <?php
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
@@ -13,4 +14,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
