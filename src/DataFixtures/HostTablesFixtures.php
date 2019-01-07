000c39494dd0cdb5de81f7ac433e5fde5038e717
mathoutitou <fougeron.mathilde@gmail.com>
2019-01-07 17:17:17 +0100

    petites modifs HostTablesFixtures

--- src/DataFixtures/HostTablesFixtures.php
+++ src/DataFixtures/HostTablesFixtures.php
@@ -13,55 +13,57 @@ class HostTablesFixtures extends Fixture
 
         $table1 = new HostTables();
         $table1
-            ->setName("La marée des envies")
+            ->setName("La Marée des Envies")
             ->setMinPrice(20)
             ->setMaxPrice(35)
             ->setCapacity(60)
-            ->setAddress("2, place de la mairie")
+            ->setAddress("2, place de la Mairie")
             ->setCity("La Baule")
             ->setNote(4.5)
-            ->setLongDescription("Magnifique table d'hôte de bord de mer")
-            ->setShortDescription("Venez découvrir")
+            ->setLongDescription("Spicy jalapeno bacon ipsum dolor amet andouille pastrami adipisicing, corned beef turkey boudin ground round proident duis rump elit. Andouille cupidatat sunt, ipsum tenderloin aliquip occaecat venison turkey esse aliqua reprehenderit est ham corned beef. Hamburger drumstick eu, nostrud elit mollit shankle pork belly ham pariatur excepteur. Exercitation t-bone ea officia, corned beef ullamco nisi irure pariatur labore esse. Pariatur occaecat deserunt pork loin. Beef ribs deserunt sed bresaola pig short loin doner exercitation ad. Ea meatball minim veniam ribeye, ham hock brisket buffalo jowl quis pork belly capicola.")
+            ->setShortDescription("Magnifique table d'hôte de bord de mer")
             ->setZipCode("44130")
-            ->setWebsite("www.superResto.com")
-            ->setTel("0629304942");
+            ->setWebsite("www.maree-envies.com")
+            ->setTel("0238202020");
 
         $manager->persist($table1);
-        $this->addReference('maree', $table1);
+        $this->addReference('table1', $table1);
 
         $table2 = new HostTables();
         $table2
-            ->setName("La toupie des prix")
+            ->setName("La Toupie du Salami")
             ->setMinPrice(10)
             ->setMaxPrice(25)
             ->setCapacity(20)
-            ->setAddress("2, place de la mairie")
-            ->setCity("La Baule")
+            ->setAddress("2, place de la Mairie")
+            ->setCity("Nantes")
             ->setNote(2.0)
-            ->setLongDescription("Magnifique table d'hôte de bord de mer")
-            ->setShortDescription("Venez découvrir")
-            ->setZipCode("44130")
-            ->setWebsite("www.superResto.com")
-            ->setTel("0629304942");
+            ->setLongDescription("Spicy jalapeno bacon ipsum dolor amet andouille pastrami adipisicing, corned beef turkey boudin ground round proident duis rump elit. Andouille cupidatat sunt, ipsum tenderloin aliquip occaecat venison turkey esse aliqua reprehenderit est ham corned beef. Hamburger drumstick eu, nostrud elit mollit shankle pork belly ham pariatur excepteur. Exercitation t-bone ea officia, corned beef ullamco nisi irure pariatur labore esse. Pariatur occaecat deserunt pork loin. Beef ribs deserunt sed bresaola pig short loin doner exercitation ad. Ea meatball minim veniam ribeye, ham hock brisket buffalo jowl quis pork belly capicola.")
+            ->setShortDescription("Venez découvrir blabla")
+            ->setZipCode("44000")
+            ->setWebsite("www.toupie-salami.com")
+            ->setTel("0238202020");
 
         $manager->persist($table2);
+        $this->addReference('table2', $table2);
 
         $table3 = new HostTables();
         $table3
-            ->setName("La tortilla de Maria")
+            ->setName("La Tortilla de Maria")
             ->setMinPrice(7)
             ->setMaxPrice(18.50)
             ->setCapacity(80)
-            ->setAddress("2, place de la mairie")
+            ->setAddress("2, place de la Mairie")
             ->setCity("La Baule")
             ->setNote(4)
-            ->setLongDescription("Magnifique table d'hôte de bord de mer")
-            ->setShortDescription("Venez découvrir")
+            ->setLongDescription("Spicy jalapeno bacon ipsum dolor amet andouille pastrami adipisicing, corned beef turkey boudin ground round proident duis rump elit. Andouille cupidatat sunt, ipsum tenderloin aliquip occaecat venison turkey esse aliqua reprehenderit est ham corned beef. Hamburger drumstick eu, nostrud elit mollit shankle pork belly ham pariatur excepteur. Exercitation t-bone ea officia, corned beef ullamco nisi irure pariatur labore esse. Pariatur occaecat deserunt pork loin. Beef ribs deserunt sed bresaola pig short loin doner exercitation ad. Ea meatball minim veniam ribeye, ham hock brisket buffalo jowl quis pork belly capicola.")
+            ->setShortDescription("Venez découvrir blabla")
             ->setZipCode("44130")
-            ->setWebsite("www.superResto.com")
-            ->setTel("0629304942");
+            ->setWebsite("www.tortilla-maria.com")
+            ->setTel("0238202020");
 
         $manager->persist($table3);
+        $this->addReference('table3', $table3);
 
         $manager->flush();
     }
