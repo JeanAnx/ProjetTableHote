a0e28766de95d41cff3e96f68bc2bcd4fe47c44f
mathoutitou <fougeron.mathilde@gmail.com>
2019-01-07 12:22:56 +0100

    création entité Hours

---
+++ src/Entity/Hours.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\HoursRepository")
+ */
+class Hours
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=25)
+     */
+    private $day;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $morningStart;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $morningEnd;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $eveningStart;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $eveningEnd;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDay(): ?string
+    {
+        return $this->day;
+    }
+
+    public function setDay(string $day): self
+    {
+        $this->day = $day;
+
+        return $this;
+    }
+
+    public function getMorningStart(): ?\DateTimeInterface
+    {
+        return $this->morningStart;
+    }
+
+    public function setMorningStart(\DateTimeInterface $morningStart): self
+    {
+        $this->morningStart = $morningStart;
+
+        return $this;
+    }
+
+    public function getMorningEnd(): ?\DateTimeInterface
+    {
+        return $this->morningEnd;
+    }
+
+    public function setMorningEnd(\DateTimeInterface $morningEnd): self
+    {
+        $this->morningEnd = $morningEnd;
+
+        return $this;
+    }
+
+    public function getEveningStart(): ?\DateTimeInterface
+    {
+        return $this->eveningStart;
+    }
+
+    public function setEveningStart(\DateTimeInterface $eveningStart): self
+    {
+        $this->eveningStart = $eveningStart;
+
+        return $this;
+    }
+
+    public function getEveningEnd(): ?\DateTimeInterface
+    {
+        return $this->eveningEnd;
+    }
+
+    public function setEveningEnd(\DateTimeInterface $eveningEnd): self
+    {
+        $this->eveningEnd = $eveningEnd;
+
+        return $this;
+    }
+}
