8610a16702324dfc471f422f28eae3d16f3401bc
mathoutitou <fougeron.mathilde@gmail.com>
2019-01-10 11:58:07 +0100

    minimum heure pour le form Réserver - page show

--- src/Form/BookingsFormType.php
+++ src/Form/BookingsFormType.php
@@ -10,12 +10,14 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Time;
 
 class BookingsFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $today = new DateTime('now');
+        $actualHour = new DateTime('now');
         $builder
             ->add('date', DateType::class, array(
                 'label' => 'Date',
@@ -27,7 +29,8 @@ class BookingsFormType extends AbstractType
             ->add('heure', TimeType::class, array(
                 'label' => 'Heure',
                 'required' => true,
-                'attr' => ['id' => 'heure'],
+                'attr' => ['id' => 'heure',
+                    'min' => $actualHour->format('H:i')],
                 'widget' => 'single_text'
             ))
         ->add('nb_convives', ChoiceType::class, array(
